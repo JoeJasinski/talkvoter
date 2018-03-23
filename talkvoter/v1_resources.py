758fae2e683bb44dddcefd1f1bf7045a39f02f75
Joe Jasinski <joe.jasinski@gmail.com>
2018-03-22 23:20:55 -0500

    Added talk to vote Abstracted out get_talk_or_abort logic

--- talkvoter/v1_resources.py
+++ talkvoter/v1_resources.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource, Api
+from flask_restful import reqparse
 from flask import Blueprint, abort
 from sqlalchemy.sql.expression import func
 from marshmallow import ValidationError
@@ -11,15 +12,21 @@ api_bp = Blueprint('api_v1', __name__)
 api = Api(api_bp)
 
 
+def get_talk_or_abort(id):
+    talk_obj = db.session.query(Talk).filter(Talk.id == id).first()
+    if not talk_obj:
+        abort(404, '`talk_id` is not in database')
+    return talk_obj
+
+
 class TalksResource(Resource):
 
     def get(self):
         schema = TalkSchema()
         data = {}
         talk_objs = db.session.query(Talk)
-        ret_code = 200
         data = schema.dump(talk_objs, many=True).data
-        return data, ret_code
+        return data, 200
 
 
 api.add_resource(TalksResource, '/talks/', endpoint="api.talks")
@@ -29,14 +36,9 @@ class TalkResource(Resource):
 
     def get(self, id):
         schema = TalkSchema()
-        data = {}
-        talk_obj = db.session.query(Talk).filter(Talk.id == id).first()
-        if not talk_obj:
-            abort(404, '`talk_id` is not in database')
-        else:
-            ret_code = 200
-            data = schema.dump(talk_obj).data
-        return data, ret_code
+        talk_obj = get_talk_or_abort(id)
+        data = schema.dump(talk_obj).data
+        return data, 200
 
 
 api.add_resource(TalkResource, '/talks/<int:id>/', endpoint="api.talk")
@@ -46,39 +48,54 @@ class TalkRandResource(Resource):
 
     def get(self):
         schema = TalkSchema()
-        data = {}
         talk_obj = db.session.query(Talk).order_by(func.random()).first()
         if not talk_obj:
             abort(404, '`talk_id` is not in database')
-        else:
-            ret_code = 200
-            data = schema.dump(talk_obj).data
-        return data, ret_code
+        data = schema.dump(talk_obj).data
+        return data, 200
 
 
 api.add_resource(TalkRandResource, '/talks/random/', endpoint="api.talkrand")
 
 
+def validate_vote_type(value):
+    if value not in [VoteValue.in_person.value, VoteValue.watch_later.value]:
+        raise ValueError("Invalid object: must be watch_later or in_person")
+    return value
+
+
 class VoteResource(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'vote', type=validate_vote_type,
+        required=True, help='Must be in_person | watch_later')
 
     def post(self, id):
         return self.get()
 
     def get(self, id):
+        talk_obj = get_talk_or_abort(id)
+        args = self.parser.parse_args()
+        vote = args['vote']
+
         schema = VoteSchema()
         msg = ""
         try:
-            obj = schema.load(
-                {'value': VoteValue.in_person.value},
+            serial_obj = schema.load(
+                {'value': vote},
                 session=db.session)
         except ValidationError as err:
             err.messages
             valid_data = err.valid_data
             data = str(err)
+            print(data, valid_data)
             ret_code = 400
             msg = "Fail"
         else:
-            db.session.add(obj.data)
+            obj = serial_obj.data
+            obj.talk = talk_obj
+            db.session.add(obj)
             db.session.commit()
             msg = "Success"
             ret_code = 200
