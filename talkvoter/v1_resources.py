d55bb2c014198c6ab2b2d66ab3177005f1afd9bf
Joe Jasinski <joe.jasinski@gmail.com>
2018-03-22 20:08:20 -0500

    Added talk list and talk detail views

--- talkvoter/v1_resources.py
+++ talkvoter/v1_resources.py
@@ -2,8 +2,8 @@ from flask_restful import Resource, Api
 from flask import Blueprint
 from marshmallow import ValidationError
 from webargs.flaskparser import use_args
-from .models import db
-from .serializers import VoteSchema
+from .models import db, Talk
+from .serializers import VoteSchema, TalkSchema
 from .constants import VoteValue
 
 
@@ -11,12 +11,44 @@ api_bp = Blueprint('api_v1', __name__)
 api = Api(api_bp)
 
 
-class VoteResource(Resource):
-
-    def post(self):
-        return self.get()
+class TalksResource(Resource):
 
     def get(self):
+        schema = TalkSchema()
+        data = {}
+        talk_objs = db.session.query(Talk)
+        ret_code = 200
+        data = schema.dump(talk_objs, many=True).data
+        return data, ret_code
+
+
+api.add_resource(TalksResource, '/talks/', endpoint="api.talks")
+
+
+class TalkResource(Resource):
+
+    def get(self, id):
+        schema = TalkSchema()
+        data = {}
+        talk_obj = db.session.query(Talk).filter(Talk.id == id).first()
+        if not talk_obj:
+            ret_code = 404
+            data = {"message": "talk not found"}
+        else:
+            ret_code = 200
+            data = schema.dump(talk_obj).data
+        return data, ret_code
+
+
+api.add_resource(TalkResource, '/talks/<int:id>/', endpoint="api.talk")
+
+
+class VoteResource(Resource):
+
+    def post(self, id):
+        return self.get()
+
+    def get(self, id):
         schema = VoteSchema()
         msg = ""
         try:
@@ -38,4 +70,4 @@ class VoteResource(Resource):
         return {"message": msg}, ret_code
 
 
-api.add_resource(VoteResource, '/talk/')
+api.add_resource(VoteResource, '/talks/<int:id>/vote/', endpoint="api.vote")
